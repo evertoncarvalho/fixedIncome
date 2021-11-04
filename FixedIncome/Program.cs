3f18e4755eb55cc4764603253a558fac07af1e79
Everton <Everton@DESKTOP-672I0RD>
2021-11-04 18:37:22 -0300

    Add project files.

---
+++ FixedIncome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FixedIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
