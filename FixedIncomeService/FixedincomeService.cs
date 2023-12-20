425d8f20eda01ec38567c66d29fcccec9ed6603b
everton.carvalho <everton.emails@gmail.com>
2023-12-20 10:03:04 -0300

    Windows Service Structure.

---
+++ FixedIncomeService/FixedincomeService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FixedIncomeService
+{
+    internal class FixedincomeService(ILogger<FixedincomeService> logger)
+        : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                logger.LogWarning($"starting {GetType().Name}");
+                while(!stoppingToken.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromMinutes(1));
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // stopping service
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "something doesnt work...");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ FixedIncomeService/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+namespace FixedIncomeService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddWindowsService(
+                service =>
+                {
+                    service.ServiceName.Equals("FixedIncomeService");
+                });
+            LoggerProviderOptions.RegisterProviderOptions<
+                EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+            //builder.Services.AddSingleton<FixedIncomeManager>();
+            builder.Services.AddHostedService<FixedIncomeService.FixedincomeService>();
+
+            IHost host = builder.Build();
+            host.Run();
+        }
+    }
+}
