2276d15541b3de93e4629e0bc6458c39766a19de
everton.carvalho <everton.emails@gmail.com>
2023-12-27 11:08:56 -0300

    Generic Server That Support FlatbufferProtocol Flatbuffer

---
+++ FlatbufferOnServer/MainServer.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace FlatbufferOnServer
+{
+    public abstract class MainServer <MessageType, Message>
+    {
+        protected delegate void MessageHandler(ProtocolControl clientSocket, Message message);
+
+        protected List<ProtocolControl> Clients = new();
+        protected Dictionary<MessageType, MessageHandler> Callbacks = new();
+        public MainServer()
+        {
+        }
+        public abstract void FillCallbacks();
+        public virtual async void Start(int listenPort, int maxSimuntaneousRequestsBeforeBusy = 10)
+        {
+            IPAddress ipAddress = IPAddress.Any;
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, listenPort);
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(endPoint);
+            listener.Listen(maxSimuntaneousRequestsBeforeBusy);
+            ProtocolControl socket;
+            while (true)
+            {
+                Socket newClient = await listener.AcceptAsync();
+                lock (Clients)
+                {
+                    socket = (ProtocolControl)Activator.CreateInstance(typeof(ProtocolControl), new object[] { newClient });
+                    Clients.Add(socket);
+                }
+                OnNewClientConnection(socket);
+            }
+        }
+        protected abstract Message GetMessageFromBytes(byte[] messageBytes);
+        protected abstract bool IsValidMessage(Message message);
+        protected abstract byte[] GetMessageBytes(MessageType type, Message message);
+        protected abstract void ProcessMessage(ProtocolControl clientSocket, Message message);
+        protected virtual async void OnNewClientConnection(ProtocolControl socket)
+        {
+            byte[] bytes = new byte[8];
+            while (true)
+            {
+                try
+                {
+                    var message = GetMessageFromBytes(
+                        socket.Receive(ref bytes));
+                    if (IsValidMessage(message))
+                    {
+                        ProcessMessage(socket, message);
+                    }
+                    else
+                    {
+                        //Logger.Debug($"invalid message received {message.MessageType} on {ServiceName}");
+                    }
+                }
+                catch (SocketException)
+                {
+                    //disconnection;
+                    //Logger.Error($"remote disconnection from {socket.ClientData} on {ServiceName}");
+                    socket.Disconnect();
+                    break;
+                }
+                catch
+                {
+                    //Logger.Error($"invalid message received {bytes.Length} bytes. disconnected on {ServiceName}.");
+                    socket.Disconnect();
+                    break;
+                }
+            }
+        }
+        protected virtual byte[] PackMessage(MessageType type, Message message)
+        {
+            return ProtocolControl.AssemblyMessage(
+                GetMessageBytes(type, message));
+        }
+    }
+}
---
+++ FlatbufferOnServer/ProtocolControl.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Sockets;
+
+namespace FlatbufferOnServer
+{
+    internal delegate void Dispose(ProtocolControl sender);
+    public class ProtocolControl
+    {
+        private DateTime _idleSince = DateTime.Now;
+        private System.Timers.Timer _heartbeat = new System.Timers.Timer();
+        internal Socket Handler { get; set; }
+        internal event Dispose OnDispose;
+        internal ProtocolControl(int heartbeatInSeconds = 30)
+        {
+            SetupHeartbeat(heartbeatInSeconds * 1000);
+        }
+        /// <summary>
+        /// Build a buffer that contains a 4bytes header with length of the message and after that
+        /// the message itself.
+        /// </summary>
+        /// <param name="bytes">Message in bytes created by the Flatbuffer message builder using function
+        /// "ToSizedArray()" from DataBuffer.</param>
+        /// <returns>4bytes header with length of the message followed by message itself</returns>
+        internal static byte[] AssemblyMessage(byte[] bytes)
+        {
+            int offset = sizeof(int);
+            byte[] buffer = new byte[offset + bytes.Length];
+            BitConverter.GetBytes(bytes.Length).CopyTo(buffer, 0);
+            bytes.CopyTo(buffer, offset);
+            return buffer;
+        }
+        internal virtual byte[] Receive(ref byte[] data)
+        {
+            Handler.Receive(
+                data,
+                0,
+                sizeof(int),
+                SocketFlags.None);
+            int size = BitConverter.ToInt32(data, 0);
+            if (size > data.Length)
+            {
+                data = new byte[size];
+            }
+            Handler.Receive(
+               data,
+               0,
+               size,
+               SocketFlags.None);
+            _idleSince = DateTime.Now;
+            return data;
+        }
+        internal void Disconnect()
+        {
+            try
+            {
+                _heartbeat.Stop();
+                _heartbeat.Dispose();
+                Handler.Close();
+                Handler.Dispose();
+            }
+            catch (Exception ex)
+            {
+                //Logger.Error(ex);
+            }
+            finally
+            {
+                OnDispose?.Invoke(this);
+            }
+        }
+        private void SetupHeartbeat(double interval)
+        {
+            _heartbeat.Interval = interval;
+            _heartbeat.Elapsed += Heartbeat_Elapsed;
+            _heartbeat.Start();
+        }
+        private bool IdleExceeded
+        {
+            get
+            {
+                return (DateTime.Now - _idleSince).Seconds > _heartbeat.Interval / 1000;
+            }
+        }
+        private void Heartbeat_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (IdleExceeded)
+            {
+                //Logger.Error($"connectionCheck: disconnecting {ClientData} due to idletime exceeded");
+                Disconnect();
+            }
+        }
+    }
+}
